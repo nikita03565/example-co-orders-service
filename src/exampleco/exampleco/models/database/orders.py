622eef0f15026d8f1f45860e0f32555fb415d670
nikita <nikita03565@gmail.com>
2022-09-27 12:25:26 +0300

    added order tables. formatted with black

---
+++ src/exampleco/exampleco/models/database/orders.py
@@ -0,0 +1,95 @@
+import enum
+
+from marshmallow import fields
+from marshmallow_sqlalchemy import SQLAlchemySchema
+from sqlalchemy import Column, Integer, String, text, TIMESTAMP, ForeignKey, Enum
+from sqlalchemy.orm import relationship
+from .services import Service
+from . import Base
+
+
+class OrderStatuses(enum.Enum):
+    ACTIVE = "ACTIVE"
+    DELETED = "DELETED"
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    service_id = Column(Integer, ForeignKey("services.id"), nullable=False)
+    service = relationship("Service", back_populates="orders")
+    order_items = relationship("OrderItem", back_populates="order")
+    status = Column(
+        Enum(OrderStatuses),
+        default=OrderStatuses.ACTIVE.value,
+        server_default=OrderStatuses.ACTIVE.value,
+        nullable=False,
+    )
+
+    created_on = Column(
+        TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    modified_on = Column(
+        TIMESTAMP,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+        server_onupdate=text("CURRENT_TIMESTAMP"),
+    )
+
+    def __repr__(self) -> str:
+        return (
+            "<Order(name='{}', status='{}', service_id='{}', created_on='{}')>".format(
+                self.name, self.status, self.service_id, self.created_on
+            )
+        )
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
+    order = relationship("Order", back_populates="order_items")
+    created_on = Column(
+        TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    modified_on = Column(
+        TIMESTAMP,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+        server_onupdate=text("CURRENT_TIMESTAMP"),
+    )
+
+    def __repr__(self) -> str:
+        return "<OrderItem(name='{}', order_id='{}', created_on='{}')>".format(
+            self.name, self.order_id, self.created_on
+        )
+
+
+class OrderSchema(SQLAlchemySchema):
+    id = fields.Integer()
+    name = fields.String(required=True)
+    service_id = fields.Integer(required=True)
+    created_on = fields.DateTime()
+    modified_on = fields.DateTime()
+
+    class Meta:
+        model = Order
+        load_instance = True
+
+
+class OrderItemSchema(SQLAlchemySchema):
+    id = fields.Integer()
+    name = fields.String(required=True)
+    order_id = fields.Integer(required=True)
+    created_on = fields.DateTime()
+    modified_on = fields.DateTime()
+
+    class Meta:
+        model = OrderItem
+        load_instance = True
